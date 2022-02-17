54097aab29003a45c57be1affdfeab47e7f617dc
Victor Torres <torresvictor100@gmail.com>
2022-02-17 14:28:27 -0300

    origin

---
+++ src/main/java/com/project/professor/allocation/victor/ProfessorAllocationVictorApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationVictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/victor/ProfessorAllocationVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.victor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
